cab549b81294b1bca4c25bfcccf47574980a8155
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-13 17:53:56 +0300

    Add the login page and design styling

--- adminlogin.php
+++ adminlogin.php
@@ -1,25 +1,26 @@
 <?php $pageTitle = 'Admin Login Page'; include("includes/header.php") ?>
-
-    <div class="container">
+<div class="login-page">
+    <div class="container py-5">
         <div class="col-md-12">
             <div class="row">
                 <div class="col-md-3">
-
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-6 bg-secondary rounded-3 p-5 text-center">
+                    <img src="assets/images/admin.png" width="100" height="100" alt="admin logo">
                     <form method="post">
-                        <div class="form-group">
-                            <label for="username"> username</label>
-                            <input type="text" name="uname">
+                        <div class="form-group p-3">
+                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
+                        </div>
+                        <div class="form-group p-3">
+                            <input type="password" name="password" class="form-control" placeholder="Enter Password">
+                            <input type="submit" class="btn btn-success mt-3" name="login" value="LOGIN">
                         </div>
                     </form>
                 </div>
-                <div class="col-md-3">
-                    
+                <div class="col-md-3">    
                 </div>
             </div>
         </div>
     </div>
-
-
+</div>
 <?php include("includes/footer.php") ?>
