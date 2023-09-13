d42079c0cdf959c8c303b8a1cf2ed56ca875558c
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-13 16:35:52 +0300

    Add the navigation and homepage styling

---
+++ adminlogin.php
@@ -0,0 +1,25 @@
+<?php $pageTitle = 'Admin Login Page'; include("includes/header.php") ?>
+
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3">
+
+                </div>
+                <div class="col-md-6">
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="username"> username</label>
+                            <input type="text" name="uname">
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php include("includes/footer.php") ?>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include("includes/header.php") ?>
+
+    <div class="container mt-5">
+        <div class="col-md-12 d-flex justify-content-between align-middle">
+            <div class="row">
+                <div class="col-md-3 mx-1 mt-1 py-3 shadow text-center">
+                    <img src="assets/images/info.png" width="215" height="215" alt="more info" class="img-fluid">
+                    <a href="#">
+                        <button class="btn btn-success my-3">More Info</button>
+                    </a>
+                </div>
+                <div class="col-md-4 mx-1 mt-1 py-3 shadow text-center">
+                    <img src="assets/images/patient.jpg" alt="patient image" class="img-fluid">
+                    <h5 class="my-2">Out and Inpatient Treatment</h5>
+                    <a href="#">
+                        <button class="btn btn-success my-3">Create An Account</button>
+                    </a>
+                </div>
+                <div class="col-md-4 mx-1 mt-1 py-3 shadow text-center">
+                    <img src="assets/images/doctorbg.jpg" alt="doctor image" class="img-fluid">
+                    <h5 class="my-2">Doctors Needed</h5>
+                    <a href="#">
+                        <button class="btn btn-success my-3">Apply Now!!!</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include("includes/footer.php") ?>
